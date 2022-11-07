332b293c981f2fd2e44220a615af651165ff48ad
phillip@premmert.de <git-sync@trilby.media>
2022-11-07 21:17:43 +0100

    (Grav GitSync) Automatic Commit from phillip@premmert.de

--- plugins/git-sync/vendor/sebastian/git/src/Git.php
+++ plugins/git-sync/vendor/sebastian/git/src/Git.php
@@ -24,6 +24,7 @@ class Git
      */
     public function __construct($repositoryPath)
     {
+	$this->grav['log']->info($repositoryPath);
         if (!is_dir($repositoryPath)) {
             throw new RuntimeException(
                 sprintf(
